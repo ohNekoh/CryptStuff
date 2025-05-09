bddbbadee0f5359287f030fe3798badb1e66a90d
Nekoh <153018922+ohNekoh@users.noreply.github.com>
2025-05-09 12:59:50 +0200

    Update cript.py

--- cript.py
+++ cript.py
@@ -6,27 +6,33 @@ import string
 # NB: Questi codici DEVONO essere cambiati se c'è la volontà di sfruttare il codice
 # Per comodità è limitato a 14 lettere, può essere esteso facilmente senza ripercussioni
 
-prefix_map = {
-    1: "UTdIO",
-    2: "IROES",
-    3: "ARIOD",
-    4: "ITOR",
-    5: "IORE",
-    6: "IYTRW",
-    7: "OTPET",
-    8: "ITHKF",
-    9: "JGEKW",
-    10: "JFKETO",
-    11: "FIOEW",
-    12: "TEODF",
-    13: "RORPE",
-    14: "IROFS",
-}
+
+
+token = input("Inserisci il token: ")
+try:
+    prefix_map = {
+        1: token[0:3],
+        2: token[3:6],
+        3: token[6:9],
+        4: token[9:12],
+        5: token[12:15],
+        6: token[15:18],
+        7: token[18:21],
+        8: token[21:24],
+        9: token[24:27],
+        10: token[27:30],
+        11: token[30:33],
+        12: token[33:36],
+        13: token[36:39],
+        14: token[39:42],
+    }
+except IndexError as ex:
+    print("Token invalido.")
 
 # queste variabili gestiscono il livello di intensità (punto a come min e punto b come max)
 # possono essere personalizzate da qui, oppure è possibile modificare la quantità direttamente dall'IF chiamato dopo l'input lenght_hide
-lvl_1_a = 20
-lvl_1_b = 50
+lvl_1_a = 5
+lvl_1_b = 10
 lvl_2_a = 50
 lvl_2_b = 100
 lvl_3_a = 100
@@ -49,7 +55,7 @@ def encrypt_message():
     message = input("Inserisci il messaggio da criptare: ")
     output_path = input("Inserisci il nome del file in cui inserire il messaggio (es: Messaggio.txt): ")
     print("Nota bene: Un livello maggiore d'intensità renderà il messaggio ancora più difficile da decifrare, ma sarà più pesante in memoria")
-    lenght_hide = input("Inserire il livello di intensità (1|5) [Default: 3]: ")
+    lenght_hide = int(input("Inserire il livello di intensità (1|5) [Default: 3]: "))
     if lenght_hide == 1:
         lenght_1 = lvl_1_a
         lenght_2 = lvl_1_b
@@ -72,7 +78,7 @@ def encrypt_message():
     encrypted_message = ""
     for word in message.split():
         for i, char in enumerate(word, start=1):
-            prefix = prefix_map.get(i, "EXTRA")
+            prefix = prefix_map.get(i, "TOGHR")
             random_chars_before = random_trash(random.randint(lenght_1, lenght_2))
             encrypted_message += f"{random_chars_before}{prefix}{char}"
 
@@ -82,5 +88,9 @@ def encrypt_message():
         file.write(encrypted_message)
     
     print(f"Messaggio salvato in: {output_path}")
+    print(f"""
+Messaggio:
+          
+          {encrypted_message}""")
 
 encrypt_message()
